e6536ea99a19a428f82395835c337c9e2c41d546
Nav Dhamu <n4vdeep@gmail.com>
2020-08-04 18:55:00 +0100

    added functionality and styles for user details section of form

---
+++ step_form/src/components/FormUserDetails.js
@@ -0,0 +1,59 @@
+import React, { Component, Fragment } from 'react';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import AppBar from 'material-ui/AppBar';
+import TextField from 'material-ui/TextField';
+import RaisedButton from 'material-ui/RaisedButton';
+
+export class FormUserDetails extends Component {
+  continue = e => {
+    e.preventDefault();
+    this.props.nextStep();
+  }
+
+  render() {
+    const { values, handleChange } = this.props;
+
+    return (
+      <MuiThemeProvider>
+        <Fragment>
+          <AppBar title="Enter User Details" />
+          <TextField
+            hintText="Enter First Name"
+            floatingLabelText="First Name"
+            onChange={handleChange('firstName')}
+            defaultValue={values.firstName}
+          />
+          <br />
+          <TextField
+            hintText="Enter Last Name"
+            floatingLabelText="Last Name"
+            onChange={handleChange('lastName')}
+            defaultValue={values.lastName}
+          />
+          <br />
+          <TextField
+            hintText="Enter Email Address"
+            floatingLabelText="Email Address"
+            onChange={handleChange('email')}
+            defaultValue={values.email}
+          />
+          <br />
+          <RaisedButton
+            label="Continue"
+            primary={true}
+            style={styles.button}
+            onClick={this.continue}
+          />
+        </Fragment>
+      </MuiThemeProvider>
+    )
+  }
+}
+
+const styles = {
+  button: {
+    margin: 15
+  }
+}
+
+export default FormUserDetails
